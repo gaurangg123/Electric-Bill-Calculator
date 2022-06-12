56c20f38120f8b6b8eb9b1f47470a20f8c82b94a
Amoghj17 <101589979+Amoghj17@users.noreply.github.com>
2022-06-12 14:28:12 +0530

    Update miniproject.c

--- miniproject.c
+++ miniproject.c
@@ -11,22 +11,7 @@ int main()
     /*
      * Calculates electricity bill according to given conditions
      */
-    if(unit <= 50)
-    {
-        amt = unit * 0.50;
-    }
-    else if(unit <= 150)
-    {
-        amt = 25 + ((unit-50)*0.75);
-    }
-    else if(unit <= 250)
-    {
-        amt = 100 + ((unit-150)*1.20);
-    }
-    else if(unit > 250)
-    {
-        amt = 220 + ((unit-250)*1.50);
-    }
+    
     /*
      * Calculates total electricity bill
      * after adding sur charges
